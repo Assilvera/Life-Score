60342d599e4f39983a0569154e750f944f75c99b
Assilvera <124600969+Assilvera@users.noreply.github.com>
2025-12-07 15:07:44 -0500

    Un solo auth

--- services/auth/src/domain/repositories/IUserRepository.ts
+++
@@ -1,8 +0,0 @@
-import { User, UserRole } from "../entities/User";
-
-export interface IUserRepository {
-  findByEmail(email: string): Promise<User | null>;
-  findByName(name: string): Promise<User | null>;
-  create(user: User): Promise<void>;
-  updateRole(userId: string, role: UserRole): Promise<void>;
-}
